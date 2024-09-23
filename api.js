b6d8de0dc4d0f56ae7eab9a45711324d605ae264
abdelkebir7y <abdouhassou92@gmail.com>
2024-09-23 14:57:54 +0100

    reverse last commit

--- api.js
+++ api.js
@@ -1,45 +1,10 @@
-const http = require("http");
-const { Server } = require("socket.io");
 const express = require("express");
-
-const sockets = require("./sockets.js");
+const path = require("path");
 
 const api = express();
 
-api.use(express.json());
+api.use(express.static(path.join(__dirname, "public")));
 
-const httpServer = http.createServer(api);
+api.use("/", express.static("index.html"));
 
-const socketServer = new Server(httpServer, { path: "/sms/socket.io" });
-
-const PORT = 4000;
-
-httpServer.listen(PORT);
-
-sockets.listen(socketServer);
-
-api.post("/sms", async (req, res) => {
-  const { phoneNumber, message } = req.body;
-
-  const otp = Math.floor(100000 + Math.random() * 900000);
-
-  const response = await fetch("https://www.sms.ma/mcms/sendsms", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      login: "ctsMarrakech",
-      password: "c@g3e87h4u",
-      oadc: "MTAXI",
-      msisdn_to: phoneNumber,
-      body: message ?? `Votre code de vérification est: ${otp}`,
-    }),
-  });
-
-  const data = await response.json();
-
-  console.log(data);
-
-  res.json(`${otp}`);
-});
+module.exports = api;
